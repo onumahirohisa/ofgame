235c49f400b0f56897b647f6ea9dcfe2a2eafdd3
kyoro <hirohisaonuma@icloud.com>
2020-09-28 18:28:02 +0900

    tab_menu

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@
 {
   const menuItems = document.querySelectorAll(".menu li a");
   const contents = document.querySelectorAll(".content");
-
   menuItems.forEach((clickedItem) => {
     clickedItem.addEventListener("click", (e) => {
       e.preventDefault();
@@ -11,11 +10,13 @@
       menuItems.forEach((item) => {
         item.classList.remove("active");
       });
+
       clickedItem.classList.add("active");
 
       contents.forEach((content) => {
         content.classList.remove("active");
       });
+
       document.getElementById(clickedItem.dataset.id).classList.add("active");
     });
   });
